da9584d5450cbd4364791ebadee2550432c8e4e3
Máté Morvai <morvai21mate@gmail.com>
2020-04-24 15:01:12 +0200

    addJsIfThings

--- Project/JavaScript/script.js
+++ Project/JavaScript/script.js
@@ -5,26 +5,45 @@ name + " 's apples";
 console.log(name + " 's apple");
 console.log("hello World");
 
+let color = 'blue';
+
 $('h1').text('We are having a 10 minutes break');
 $('p').html('Stay at home, Likes:<strong>13</span>')
 $('strong').css('background', 'lime');
-$('h2').toggleClass('blue');
+$('h2').toggleClass(color);
+/*$('h2').toggleClass('blue');   ha duplázzuk törli a parancsot*/
 $('ul').append('<li>My name is' + name + '</li>');
 $('ul').append('<li>My name is' + name + '</li>');
 $('ul').append('<li id= "special-list-item">Removable' + name + '</li>');
 
-$('#special-list-item').remove();
+/*$('#special-list-item').remove(); ---> ezzel törlünk
 
 
 
-/*$('li').remove();*/
+/*$('li').remove(); ---> mindent töröl ami list(li)*/
 
-$('#addButton').click(() => {
+let addButtonHandler = () => {
+    if ($('li').length < 5) {
     $('ul').append('<li>My name is' + name + '</li>');
-});
+    }
+};
+
+$('#addButton').click(addButtonHandler);
 $('#removeButton').click(() => {
     $('li:last-child').remove('');
 });
+
+let age = 29;
+
+console.log('Starting the if');
+
+if (age < 18) {
+console.log('not old enough to drink');
+} else {
+console.log('Old enough to drink');
+}
+
+console.log(' Closing the if')
 
 
 
